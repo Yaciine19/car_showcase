3e2e6059fe194fbb64f1f910a877d065495ccd06
yacine <yacineddd32@gmail.com>
2025-05-26 17:45:21 +0100

    Make change on CustomButton and Add CarCard component - Add utils folder

--- types/index.ts
+++ types/index.ts
@@ -5,9 +5,24 @@ export interface CustomButtonProps {
   containerStyles?: string;
   handleClick?: MouseEventHandler<HTMLButtonElement>;
   btnType?: "button" | "submit";
+  textStyles?: string;
+  rightIcon?: string;
+  isDisabled?: boolean;
 }
 
 export interface SearchManufacturerProps {
   manufacturer: string;
   setManufacturer: (manufacturer: string) => void;
+}
+
+export interface CarProps {
+  class: string;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
 }
